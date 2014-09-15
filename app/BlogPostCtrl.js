bf14fe4ec76f84df4c42a4f6d7e7dbcd9b5e3b04
Dawid Tadel <dawid.tadel@gmail.com>
2014-09-15 15:28:37 +0200

    Angular exercise1 D.Tadel

--- app/BlogPostCtrl.js
+++ app/BlogPostCtrl.js
@@ -5,5 +5,7 @@
     }
 
     var module = angular.module("exerciseApp", []);
-    module.controller('BlogPostCtrl', [BlogPostCtrl]);
+    module.controller('BlogPostCtrl', function() {
+        this.posts = [{id: 1, name: 'John', pets: 'Dog'}, {id: 2, name: 'Tom', pets: 'Cat'}]
+    });
 })();
